f744104c4b418146337e5622bd1de87028a56ac6
twei7 <twei7@wisc.edu>
2017-04-28 01:20:45 +0800

    Add files via upload

--- seaf-share.py
+++ seaf-share.py
@@ -4,14 +4,99 @@ import httplib
 import re
 import getpass
 import urllib
-from clint.textui import progress
-import requests
+import imp
+import urllib2
+import urlparse
+try:
+	imp.find_module('clint')
+	clint_exist = True
+	from clint.textui import progress
+except ImportError:
+	clint_exist = False
+try:
+	imp.find_module('requests')
+	requests_exist = True
+	import requests
+except ImportError:
+	requests_exist = False
 import os
 import sys
 import json
-from requests_toolbelt import MultipartEncoder, MultipartEncoderMonitor
+try:
+	imp.find_module('requests_toolbelt')
+	requests_toolbelt_exist = True
+	from requests_toolbelt import MultipartEncoder, MultipartEncoderMonitor
+except ImportError:
+	requests_toolbelt_exist = False
+import itertools
+import mimetools
+import mimetypes
 
-def downfile(url, path, name, headers):
+class MultiPartForm(object):
+    """Accumulate the data to be used when posting a form."""
+
+    def __init__(self):
+        self.form_fields = []
+        self.files = []
+        self.boundary = mimetools.choose_boundary()
+        return
+    
+    def get_content_type(self):
+        return 'multipart/form-data; boundary=%s' % self.boundary
+
+    def add_field(self, name, value):
+        """Add a simple field to the form data."""
+        self.form_fields.append((name, value))
+        return
+
+    def add_file(self, fieldname, filename, fileHandle, mimetype=None):
+        """Add a file to be uploaded."""
+        body = fileHandle.read()
+        if mimetype is None:
+            mimetype = mimetypes.guess_type(filename)[0] or 'application/octet-stream'
+        self.files.append((fieldname, filename, mimetype, body))
+        return
+    
+    def __str__(self):
+        """Return a string representing the form data, including attached files."""
+        # Build a list of lists, each containing "lines" of the
+        # request.  Each part is separated by a boundary string.
+        # Once the list is built, return a string where each
+        # line is separated by '\r\n'.  
+        parts = []
+        part_boundary = '--' + self.boundary
+        
+        # Add the form fields
+        parts.extend(
+            [ part_boundary,
+              'Content-Disposition: form-data; name="%s"' % name,
+              '',
+              value,
+            ]
+            for name, value in self.form_fields
+            )
+        
+        # Add the files to upload
+        parts.extend(
+            [ part_boundary,
+              'Content-Disposition: form-data; name="%s"; filename="%s"' % \
+                 (field_name, filename),
+              'Content-Type: %s' % content_type,
+              '',
+              body,
+            ]
+            for field_name, filename, content_type, body in self.files
+            )
+        
+        # Flatten the list and add closing boundary marker,
+        # then return CR+LF separated data
+        flattened = list(itertools.chain(*parts))
+        flattened.append('--' + self.boundary + '--')
+        flattened.append('')
+        return '\r\n'.join(flattened)
+
+
+def downfile1(url, path, name, headers):
     r = requests.get(url, stream=True, headers=headers)
     print path+"/"+name
     try:
@@ -28,7 +113,18 @@ def downfile(url, path, name, headers):
         print "name: "+name
         print headers
 
-def downlist(files, set_headers, conn, connectPath, downpath, baseURL):
+def downfile2(url, path, name, headers):
+	sys.stdout.write("Downloading: "+path+"/"+name)
+	sys.stdout.flush()
+	request = urllib2.Request(url, headers=headers)
+	response = urllib2.urlopen(request)
+	data = response.read()
+	f = open(path+"/"+name, 'wb')
+	f.write(data)
+	f.flush()
+	print " -- OK"
+
+def downlist(files, set_headers, conn, connectPath, downpath, baseURL, updown_method):
     for (t1, t2) in files:
         if t1[-9:-1] == "mode=lis":
             #print t2+" is a dir"
@@ -40,12 +136,17 @@ def downlist(files, set_headers, conn, connectPath, downpath, baseURL):
             if os.path.exists(downpath+"/"+t2) == False:
                 os.mkdir(downpath+"/"+t2)
             #print "dir: "+downpath+"/"+t2
-            downlist(subfiles, set_headers, conn, connectPath, downpath+"/"+t2, baseURL)            
+            downlist(subfiles, set_headers, conn, connectPath, downpath+"/"+t2, baseURL, updown_method)            
         else:
             #print t2+" is a file"
             #print t1
             #print "down: "+baseURL+t1+"&dl=1"
-            downfile(baseURL+t1+"&dl=1", downpath, t2, set_headers)
+            if updown_method == 1:
+            	downfile1(baseURL+t1+"&dl=1", downpath, t2, set_headers)
+            elif updown_method == 2:
+            	downfile2(baseURL+t1+"&dl=1", downpath, t2, set_headers)
+            else:
+            	raise Exception('invalid upload/download method')
             
 def usage():
     print "seaf-share.py [get|put] [http|https]://share_url directory"
@@ -141,7 +242,7 @@ def getFile(resp, base_url, savePath, set_headers):
         downfile(base_url+"?dl=1", savePath, filename[0], set_headers)
         return True
 
-def getFiles(serverInfo, savePath):
+def getFiles(serverInfo, savePath, updown_method):
     if os.path.isdir(savePath) == False:
         print "The saving path ("+savePath+") does not exist. Please specify a valid saving path."
         sys.exit()
@@ -168,7 +269,7 @@ def getFiles(serverInfo, savePath):
 
     if os.path.exists(downpath) == False:
         os.mkdir(downpath)
-    downlist(files, set_headers, conn, serverInfo["connectPath"], downpath, baseURL)
+    downlist(files, set_headers, conn, serverInfo["connectPath"], downpath, baseURL, updown_method)
 
 def getUploadLink(conn, uploadquery):
     set_headers = {"Accept": "application/json",
@@ -195,7 +296,7 @@ def create_callback(encoder_len):
     return my_callback
 
 
-def uploadFile(file_dir, file_name, upload_dir, posturl, parent_dir, set_headers):
+def uploadFile1(file_dir, file_name, upload_dir, posturl, parent_dir, set_headers):
     print "Uploading: "+str(file_dir+"/"+file_name).replace("//", "/")
 
     e = MultipartEncoder(fields={'filename': file_name,
@@ -209,16 +310,45 @@ def uploadFile(file_dir, file_name, upload_dir, posturl, parent_dir, set_headers
     r = requests.post(posturl, data=set_params, headers=set_headers)
     #data = json.loads(r.content)
     callback(-1)
+
+def uploadFile2(conn, file_dir, file_name, upload_dir, posturl, parent_dir, set_headers):
+    sys.stdout.write("Uploading: "+str(file_dir+"/"+file_name).replace("//", "/"))
+    sys.stdout.flush()
     
-def uploadDir(upload_dir, posturl, parent_dir, base_dir, set_headers):
+    form = MultiPartForm()
+    form.add_field('filename', file_name)
+    form.add_file('file', file_name, 
+                  fileHandle=open(file_dir+"/"+file_name, 'rb'), mimetype = 'text/plain')
+    form.add_field('parent_dir', parent_dir)
+    form.add_field('relative_path', upload_dir)
+    
+    body = str(form)
+    posturl = posturl.encode('ascii') if isinstance(posturl, unicode) else posturl
+    
+    request = urllib2.Request(posturl)
+    for k,v in set_headers.items():
+		request.add_header(k, v)
+    request.add_header('Content-type', form.get_content_type())
+    request.add_header('Content-length', str(len(body)))
+    request.add_data(body)
+    urllib2.urlopen(request)
+	
+    print " -- OK"
+    
+def uploadDir(conn, upload_dir, posturl, parent_dir, base_dir, set_headers, updown_method):
     dirlist = os.listdir(upload_dir)
     for f in dirlist:
         if os.path.isfile(upload_dir+"/"+f):
-            uploadFile(upload_dir, f, base_dir, posturl, parent_dir, set_headers)
+			if updown_method == 1:
+				uploadFile1(upload_dir, f, base_dir, posturl, parent_dir, set_headers)
+			elif updown_method == 2:
+				uploadFile2(conn, upload_dir, f, base_dir, posturl, parent_dir, set_headers)
+			else:
+				raise Exception('invalid upload/download method')
         elif os.path.isdir(upload_dir+"/"+f):
-            uploadDir(upload_dir+"/"+f, posturl, parent_dir, base_dir+"/"+f, set_headers)
+			uploadDir(conn, upload_dir+"/"+f, posturl, parent_dir, base_dir+"/"+f, set_headers, updown_method)
 
-def putFiles(serverInfo, getPath):
+def putFiles(serverInfo, getPath, updown_method):
     if os.path.exists(getPath) == False:
         print "The uploading target does not exist"
         sys.exit()
@@ -246,12 +376,22 @@ def putFiles(serverInfo, getPath):
         sys.exit()
 
     if os.path.isdir(getPath):
-        uploadDir(getPath, uploadLink, parent_dir[0], os.path.basename(getPath), set_headers)
+        uploadDir(conn, getPath, uploadLink, parent_dir[0], os.path.basename(getPath), set_headers, updown_method)
     elif os.path.isfile(getPath):
-        uploadFile(os.path.dirname(getPath), os.path.basename(getPath), "", uploadLink, parent_dir[0], set_headers)
+    	if updown_method == 1:
+        	uploadFile1(os.path.dirname(getPath), os.path.basename(getPath), "", uploadLink, parent_dir[0], set_headers)
+        elif updown_method == 2:
+        	uploadFile2(conn, os.path.dirname(getPath), os.path.basename(getPath), "", uploadLink, parent_dir[0], set_headers)
+        else:
+        	raise Exception('invalid upload/download method')
 
 
 def main():
+    if (clint_exist == False) or (requests_exist == False) or (requests_toolbelt_exist == False):
+        updown_method = 2
+    else:
+    	updown_method = 1
+
     if len(sys.argv) < 4:
         usage()
     elif sys.argv[1] == "get":
@@ -260,14 +400,14 @@ def main():
             usage()
         else:
             savePath = os.path.abspath(sys.argv[3])
-            getFiles(ret, savePath)
+            getFiles(ret, savePath, updown_method)
     elif sys.argv[1] == "put":
         ret = parseURL(sys.argv[2])
         if ret == -1:
             usage()
         else:
             uploadPath = os.path.abspath(sys.argv[3])
-            putFiles(ret, uploadPath)
+            putFiles(ret, uploadPath, updown_method)
     else:
         usage()
     sys.exit();
@@ -275,11 +415,3 @@ def main():
 if __name__ == "__main__": main()
 
     
-
-
-
-
-
-
-
-
